6faf8ecf625a1c6c58743f2c8cf7dc95ce5151d5
jinhwan <ghks248@naver.com>
2024-11-15 14:35:25 +0900

    springboot jenkins

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Spring Boot + React + NCP</h1>
+            <h3>카운트 : 1</h3>
+            <img src="./image/스폰지밥.gif" alt="스폰지밥"/>
+        </div>
+    );
+};
+
+export default App;
